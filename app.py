e1a903491d522075083163f9f0781c9439dcaaaf
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-09-11 07:38:49 +0300

    adds the functionality to send emails

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+import smtplib
+import os
 
 
 app = Flask(__name__)
+my_email = "mgm.engineeringtie847@gmail.com"
+gmail_password = os.environ.get("GMAIL_PASSWORD")
 
 
 # current_year = get_current_year()
@@ -40,12 +44,21 @@ def contact():
         email = request.form.get("email")
         message = request.form.get("message")
 
-        # Basic validation
-        if not name or not email or not message:
-            return jsonify({"error": "All fields are required!"}), 400
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=gmail_password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg="Subject:Contact\n\n{}\n\n{}\n\n{}".format(name, email, message)
+        )
 
-        print(name, email, message)
-        return jsonify({"message": "Message successfully sent"}), 200
+        # # Basic validation
+        # if not name or not email or not message:
+        #     return jsonify({"error": "All fields are required!"}), 400
+
+        # return jsonify({"message": "Message successfully sent"}), 200
+        return render_template("contact.html")
 
     return render_template("contact.html")
 
