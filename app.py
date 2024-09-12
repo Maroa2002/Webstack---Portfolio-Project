b21cb2fec3b6ed01a796bf73d4afb6044ddcb8a5
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-09-12 12:31:18 +0300

    added the model for the user table

--- app.py
+++ app.py
@@ -32,6 +32,14 @@ class Post(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
+# User table model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+
 @app.route("/")
 def retrieve_all_posts():
     posts = Post.query.all()
